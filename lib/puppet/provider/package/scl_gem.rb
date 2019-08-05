5f52fbef821cb4000c32a1f84739a1567912a6b6
Trey Dockendorf <tdockendorf@osc.edu>
2019-08-05 14:00:03 -0400

    Fix rubocop issue

--- lib/puppet/provider/package/scl_gem.rb
+++ lib/puppet/provider/package/scl_gem.rb
@@ -1,7 +1,7 @@
 require 'puppet/provider/package'
 
 Puppet::Type.type(:package).provide :scl_gem, parent: :gem do
-  desc "Run gem command from SCL"
+  desc 'Run gem command from SCL'
   has_feature :versionable, :install_options
   commands gemcmd: '/usr/local/bin/scl_gem'
 end
