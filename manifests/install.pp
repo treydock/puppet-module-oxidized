dc5286020b0007f13a1ae173a5c4308e7c9a3821
treydock <tdockendorf@osc.edu>
2022-08-12 08:37:26 -0400

    Several updates to OS support and module dependencies (#27)
    * Several updates to OS and module dependencies
    * Drop CentOS 8 , replace with Rocky/AlmaLinux 8
    * Support puppet/epel 5.x
    * Use puppet/systemd module instead of camptocamp/systemd
    * Support puppetlabs/stdlib 8.x
    * Use rh-ruby27 for EL7
    * Need cmake3 for EL7
    * Drop Debian 9 support

--- manifests/install.pp
+++ manifests/install.pp
@@ -14,9 +14,9 @@ class oxidized::install {
   if $facts.dig('os', 'family') == 'RedHat' {
     if versioncmp($facts['os']['release']['major'], '8') >= 0 {
       $provider = 'system_gem'
-    }
-    else {
+    } else {
       $provider = 'scl_gem'
+      package { 'cmake': ensure => 'absent' }
       file { '/usr/local/bin/scl_gem':
         ensure => 'file',
         owner  => 'root',
@@ -24,6 +24,21 @@ class oxidized::install {
         mode   => '0755',
         source => 'puppet:///modules/oxidized/scl_gem',
         before => Package['oxidized'],
+      }
+      alternative_entry { '/usr/bin/cmake3':
+        ensure   => 'present',
+        altlink  => '/usr/bin/cmake',
+        altname  => 'cmake',
+        priority => 10,
+        require  => [
+          Package['cmake'],
+          Package['cmake3'],
+        ],
+      }
+      alternatives { 'cmake':
+        path    => '/usr/bin/cmake3',
+        require => Alternative_entry['/usr/bin/cmake3'],
+        before  => Package['oxidized'],
       }
     }
   } else {
--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,21 +1,22 @@
 # @summary Manage repos needed for oxidized
 # @api private
 class oxidized::repo {
-  if $facts['os']['family'] == 'RedHat' and versioncmp($facts['os']['release']['major'], '8') >= 0 {
+  if $facts['os']['family'] == 'RedHat' {
     include epel
     $oxidized::install_dependencies.each |$package| {
       Yumrepo['epel'] -> Package[$package]
     }
-  } elsif $facts['os']['family'] == 'RedHat' {
-    if $facts['os']['name'] == 'RedHat' {
-      rh_repo { "rhel-server-rhscl-${facts['os']['release']['major']}-rpms":
-        ensure => 'present',
-        before => Package[$::oxidized::ruby_dependencies],
-      }
-    } else {
-      package { 'centos-release-scl':
-        ensure => 'installed',
-        before => Package[$::oxidized::ruby_dependencies],
+    if versioncmp($facts['os']['release']['major'], '7') == 0 {
+      if $facts['os']['name'] == 'RedHat' {
+        rh_repo { "rhel-server-rhscl-${facts['os']['release']['major']}-rpms":
+          ensure => 'present',
+          before => Package[$::oxidized::ruby_dependencies],
+        }
+      } else {
+        package { 'centos-release-scl':
+          ensure => 'installed',
+          before => Package[$::oxidized::ruby_dependencies],
+        }
       }
     }
   }
